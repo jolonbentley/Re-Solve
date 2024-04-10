d1b91dedaa078b4127e828fbd156d011dab1dbdd
maitri-thakor <maitrithakor@gmail.com>
2024-04-10 17:35:01 +1200

    Completed client side routing

---
+++ client/pages/challenge.tsx
@@ -0,0 +1,7 @@
+export default function Challenge() {
+  return (
+    <div>
+      <h1>Welcome to Re:Solve Challenge page</h1>
+    </div>
+  )
+}
--- client/pages/home.tsx
+++ client/pages/home.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
-  return
-  ;<div>
-    <h1>Welcome to Re:Solve</h1>
-    <h2>Sign up today to get started with challenges</h2>
-  </div>
+  return (
+    <div>
+      <h1>Welcome to Re:Solve</h1>
+      <h2>Sign up today to get started with challenges</h2>
+    </div>
+  )
 }
