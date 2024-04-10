da09af2dc6f4c4bb1b725cc66880c83ccf0542ae
jolonbentley <jolon88@hotmail.com>
2024-04-11 11:48:28 +1200

    comment routes added

--- server/db/db-functions.ts
+++ server/db/db-functions.ts
@@ -1,6 +1,6 @@
 import connection from './connection'
 import { Challenge } from '../../models/challenges.ts'
-import { Solution } from '../../models/solutions.ts'
+import { Solution, SolutionComments } from '../../models/solutions.ts'
 
 const db = connection
 
@@ -24,6 +24,25 @@ export async function getAllSolutions() {
   return await db('solutions').select('*')
 }
 
+export async function getAllSolutionComments() {
+  return await db('solution_comments').select('*')
+}
+
 export async function saveSolution(data: object): Promise<Solution[]> {
   return await db('solutions').insert([data])
+}
+
+export async function updateSolution(id: number, updates: Partial<Solution>) {
+  await db('solutions').where('id', id).update(updates)
+}
+
+export async function saveComment(data: object): Promise<SolutionComments[]> {
+  return await db('solution_comments').insert([data])
+}
+
+export async function updateComment(
+  id: number,
+  updates: Partial<SolutionComments>,
+) {
+  await db('solution_comments').where('id', id).update(updates)
 }
