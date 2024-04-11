f6d3ba32498e6c0ec80124191b967e04e43605bc
Lewis Steentjes <lpsteentjes@gmail.com>
2024-04-11 12:50:43 +1200

    tidy up a file

--- client/hooks/useUsers.ts
+++
@@ -1,7 +0,0 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { useMutation } from "@tanstack/react-query";
-import { useQuery } from "@tanstack/react-query";
-import { useQueryClient } from "@tanstack/react-query";
-import { MutateFunction } from "@tanstack/react-query";
-
-export const useUser
