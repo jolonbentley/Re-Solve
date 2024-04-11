06bcba9a2747b521c53177d52c1e5cddf3b4400b
Lewis Steentjes <lpsteentjes@gmail.com>
2024-04-12 09:20:45 +1200

    Update duck generator

--- server/db/functions/users.ts
+++ server/db/functions/users.ts
@@ -12,7 +12,7 @@ function duckGenerator(seed: string) {
     'Pacific Black',
     'Shoveler',
     'Pateke',
-    'Auckland Islands Teal',
+    'Donald Duck',
     'Grey Teal']
   let hash = 0;
   for (let i = 0; i < seed.length; i++) {
