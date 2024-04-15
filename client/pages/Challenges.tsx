51c41db863d932ae67e9a12eed5c7286cc64c2e7
Lewis Steentjes <lpsteentjes@gmail.com>
2024-04-15 12:37:59 +1200

    Restyle /challenges/ page and rename /challenge/ to /challenges/

--- client/pages/Challenges.tsx
+++ client/pages/Challenges.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { fetchChallenges } from '../apis/apiClient'
+import HeadingBlock from '../components/BuildingBlocks/HeadingBlock'
+import ChallengeCard from '../components/BuildingBlocks/ChallengeCard'
+import GenericContainer from '../components/BuildingBlocks/GenericContainer'
 
 export default function Challenges() {
   const {
     isLoading,
     isError,
-    data: ChallengesData,
+    data: challengesData,
   } = useQuery({
     queryKey: ['AllChallenges'],
     queryFn: () => fetchChallenges(),
@@ -15,51 +18,16 @@ export default function Challenges() {
     return <h1>Loading...ChallengesDataPage</h1>
   }
 
-  if (isError || !ChallengesData) {
+  if (isError || !challengesData) {
     return <h1>Error</h1>
-  }
+  }  
+  
   return (
-    <div>
-      <div>
-        <h1>Challenges</h1>
-        <Link to={`/`}>
-          <button>Back</button>
-        </Link>
-      </div>
-      <div>
-        <table className="table border-separate space-y-6 text-sm text-gray-400">
-          <thead className="bg-yellow-500 text-white">
-            <tr>
-              <th className="p-3">Challenge</th>
-              <th className="p-3 text-left">Difficulty</th>
-              <th className="p-3 text-left">Upvote</th>
-              <th className="p-3 text-left">Downvotes</th>
-              <th>Solutions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ChallengesData.map((challenge) => (
-              <tr key={challenge.id} className="bg-blue-200 lg:text-black">
-                <td className="p-3">
-                  <span className="rounded-md bg-blue-400 px-2 text-gray-50">
-                    <Link to={`/challenge/${challenge.id}`}>
-                      {challenge.title}
-                    </Link>
-                  </span>
-                </td>
-                <td className="p-3">{challenge.difficulty}</td>
-                <td className="p-3">{challenge.upvotes}</td>
-                <td className="p-3">{challenge.downvotes}</td>
-                <td className="p-3">
-                  <span className="rounded-md bg-blue-400 px-2 text-gray-50">
-                    <Link to={`/solution/${challenge.id}`}>➡</Link>
-                  </span>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <GenericContainer>
+      <HeadingBlock>
+        List of All Challenges
+      </HeadingBlock>
+      {challengesData?.map((challenge, index) => ( <ChallengeCard key={index} { ...challenge }/>))}
+    </GenericContainer>
   )
 }
