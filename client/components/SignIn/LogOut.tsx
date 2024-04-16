68802d7068c50a52bc65b7dda74ad01327ef1f46
Lewis Steentjes <lpsteentjes@gmail.com>
2024-04-16 15:49:04 +1200

    Start working on nav

--- client/components/SignIn/LogOut.tsx
+++ client/components/SignIn/LogOut.tsx
@@ -1,19 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import useUser from '../../hooks/useUser'
+import { Link } from 'react-router-dom'
 
 export default function LogOut() {
   const { logout, user } = useAuth0()
-
+  const userData = useUser().data
+  const profileAddress = `/profile/${userData?.id}`
   return (
-    <div className="flex flex-row-reverse content-center items-center">
+    <div className="flex flex-col content-center items-center gap-1">
+      <Link to={profileAddress}>
+        <img src={user?.picture} className='rounded-lg w-20'></img>
+      </Link>
       <button
         onClick={() =>
           logout({ logoutParams: { returnTo: window.location.origin } })
         }
-        className="btn bg-accent text-accent-content drop-shadow-[0_2px_4px_rgba(0,0,0,0.15)]"
+        className="btn-sm bg-primary rounded-md text-primary-content drop-shadow-[0_2px_4px_rgba(0,0,0,0.15)]"
       >
         Log Out
       </button>
-      <span className="mr-2">Hello, {user?.given_name}</span>
     </div>
   )
 }
