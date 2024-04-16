75c6377c1adf673305e2a8a43d5af0bf8ce928a3
jolonbentley <jolon88@hotmail.com>
2024-04-17 11:45:50 +1200

    profile styling updated

--- client/components/MyProfile.tsx
+++ client/components/MyProfile.tsx
@@ -10,32 +10,32 @@ export function MyProfile() {
 
   return (
     <div className="profile-container">
-      <div className="profile-header">
-        <div className="profile-avatar">
-          <img src={user?.profile_pic_url} alt="user portrait" />
-        </div>
-        <div className="profile-info">
-          <h2 className="p-name">{user?.name}</h2>
-          <div className="profile-details">
-            <p>Experience: {user?.experience}</p>
-            <p>Coolness: {user?.coolness}</p>
-            <p>Spaghetness: {user?.spaghetness}</p>
-            <p>Favourite Duck: {user?.favourite_duck}</p>
-            <p>About: {user?.about}</p>
+      <div className="main-profile-header">
+        <div className="profile-header">
+          <div className="profile-avatar">
+            <img src={user?.profile_pic_url} alt="user portrait" />
+          </div>
+          <div className="profile-info">
+            <h2 className="p-name">{user?.name}</h2>
+            <div className="profile-details">
+              <p>Experience: {user?.experience}</p>
+              <p>Coolness: {user?.coolness}</p>
+              <p>Spaghetness: {user?.spaghetness}</p>
+              <p>Favourite Duck: {user?.favourite_duck}</p>
+              <p>About: {user?.about}</p>
+            </div>
           </div>
         </div>
+        <div className="btn-profile-div">
+          <Link to="/profile/edit">
+            <button className="btn bg-accent text-accent-content drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:bg-gray-400">
+              Edit Profile
+            </button>
+          </Link>
+        </div>
       </div>
       <div>
-        <Link to="/profile/edit">
-          <button className="btn bg-accent text-accent-content drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] hover:bg-gray-400">
-            Edit Profile
-          </button>
-        </Link>
-      </div>
-      <div>
-        <HeadingBlock>
-          <CompletedChallengesByUserId />
-        </HeadingBlock>
+        <CompletedChallengesByUserId />
       </div>
     </div>
   )
