b5454caae8d30b25b0267f5530bcfae5cab6ac3e
Tyler McBride <imtylermcbride@gmail.com>
2024-04-17 16:11:23 +1200

    added default picture to user registration, removed console logging

--- client/components/UserProfile.tsx
+++ client/components/UserProfile.tsx
@@ -2,10 +2,8 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { getUserById } from '../apis/userAPI'
 import { CompletedChallengesByUserId } from './CompletedChallengesByuserId'
-import HeadingBlock from './BuildingBlocks/HeadingBlock'
 
 export function UserProfile() {
-  // const user = useUser().data
   const id = Number(useParams().id)
 
   const {
@@ -25,10 +23,6 @@ export function UserProfile() {
   if (isError) {
     return <div>There was an error, {String(error)}</div>
   }
-
-  console.log(user)
-
-  // NEED TO ADD THEIR SOLUTIONS TABLE TO THEIR PROFILE
 
   return (
     <div className="profile-container">
